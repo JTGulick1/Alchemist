7d935707d11b93bad43477dfc3909c31d1b12d59
JT Gulick <jtgulick1@gmail.com>
2024-03-19 15:38:40 -0400

    Added A closed time and days of the week

--- Alchemist/Assets/Scripts/UI/WorldTimer.cs
+++ Alchemist/Assets/Scripts/UI/WorldTimer.cs
@@ -7,12 +7,15 @@ public class WorldTimer : MonoBehaviour
 {
     [Header("Time")]
     public TMPro.TMP_Text timeShown;
+    public TMPro.TMP_Text closedtxt;
+    public TMPro.TMP_Text dayOfWeek;
     private int single;
     private int ten;
     private int hrs = 6;
     public float tempTime;
     private bool PM = false;
-    private bool day;
+    private bool closed = false;
+    private int day = 0;
     private int custCount = 0;
     public GameObject[] Customers;
     public GameObject CustSpawn;
@@ -28,10 +31,91 @@ public class WorldTimer : MonoBehaviour
 
     void StartDay()
     {
-        day = true;
         timeShown.text = "[" + hrs + ":" + ten + single + "]";
+        closedtxt.gameObject.SetActive(false);
+        closed = false;
         SpawnCust();
+        UpdateDay();
     }
+
+
+
+    void TimeUpdate()
+    {
+        tempTime += Time.deltaTime;
+        if (tempTime >= 10.0f)
+        {
+            ten++;
+            tempTime = 0.0f;
+            timeShown.text = "[" + hrs + ":" + ten + single + "]";
+        }
+        if (ten == 6)
+        {
+            ten = 0;
+            hrs++;
+            timeShown.text = "[" + hrs + ":" + ten + single + "]";
+            if (closed == false)
+            {
+                SpawnCust();
+            }
+        }
+        if (hrs == 13)
+        {
+            PM = true;
+            hrs = 1;
+            timeShown.text = "[" + hrs + ":" + ten + single + "]";
+        }
+        if (hrs == 8 && PM == true)
+        {
+            closed = true;
+            closedtxt.gameObject.SetActive(true);
+        }
+        if (hrs == 13 && PM == true)
+        {
+            hrs = 1;
+            timeShown.text = "[" + hrs + ":" + ten + single + "]";
+            UpdateDay();
+        }
+    }
+
+    public void UpdateDay()
+    {
+        day++;
+        if (day == 8)
+        {
+            day = 1;
+        }
+        if (day == 1)
+        {
+            dayOfWeek.text = "Monday";
+        }
+        if (day == 2)
+        {
+            dayOfWeek.text = "Tuesday";
+        }
+        if (day == 3)
+        {
+            dayOfWeek.text = "Wednesday";
+        }
+        if (day == 4)
+        {
+            dayOfWeek.text = "Thursday";
+        }
+        if (day == 5)
+        {
+            dayOfWeek.text = "Friday";
+        }
+        if (day == 6)
+        {
+            dayOfWeek.text = "Saturday";
+        }
+        if (day == 7)
+        {
+            dayOfWeek.text = "Sunday";
+        }
+    }
+
+    // Customers Infomation Systems
 
     void SpawnCust()
     {
@@ -46,29 +130,5 @@ public class WorldTimer : MonoBehaviour
     public void CustLeft()
     {
         custCount -= 1;
-    }
-
-    void TimeUpdate()
-    {
-        tempTime += Time.deltaTime;
-        if (tempTime >= 10.0f && day == true)
-        {
-            ten++;
-            tempTime = 0.0f;
-            timeShown.text = "[" + hrs + ":" + ten + single + "]";
-        }
-        if (ten == 6)
-        {
-            ten = 0;
-            hrs++;
-            timeShown.text = "[" + hrs + ":" + ten + single + "]";
-            SpawnCust();
-        }
-        if (hrs == 13)
-        {
-            PM = true;
-            hrs = 1;
-            timeShown.text = "[" + hrs + ":" + ten + single + "]";
-        }
     }
 }
