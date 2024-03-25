0e9829f934587710ec2c8b336731474caa27e29f
JT Gulick <jtgulick1@gmail.com>
2024-03-25 11:10:42 -0400

    Added Main Menu

---
+++ Alchemist/Assets/Scripts/MainMenu/MenuUI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuUI : MonoBehaviour
+{
+    public GameObject MenuCan;
+    public GameObject SavesCan;
+    public GameObject optionsCan;
+
+    private void Start()
+    {
+        SavesCan.SetActive(false);
+        MenuCan.SetActive(true);
+        optionsCan.SetActive(false);
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void OpenLoadMenu()
+    {
+        SavesCan.SetActive(true);
+        MenuCan.SetActive(false);
+    }
+
+    public void OpenSave(int n)
+    {
+
+    }
+
+    public void OpenOptions()
+    {
+        MenuCan.SetActive(false);
+        optionsCan.SetActive(true);
+    }
+
+    public void OpenMenu()
+    {
+        SavesCan.SetActive(false);
+        MenuCan.SetActive(true);
+        optionsCan.SetActive(false);
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+}
